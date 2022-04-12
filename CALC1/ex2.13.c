55924ea777f274e14bb4dd0e4f76e06f80bf1520
AMANDA MOREIRA <bv3024679@AD.SBV.IFSP.EDU.BR>
2022-04-12 16:31:14 -0300

    Esqueci de salvar 1

---
+++ CALC1/ex2.13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    float av1;
+    float av2;
+    float avOptativa;
+
+    printf("Nota Av. 1: ");
+    scanf("%f", &av1);
+
+    printf("Nota Av. 2: ");
+    scanf("%f", &av2);
+
+    printf("Nota Optativa: ");
+    scanf("%f", &avOptativa);
+
+
+
+    return 0;
+}
