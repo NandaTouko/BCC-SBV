be40d462fc43e38886ceb8095cf9d1e235d309d3
AMANDA MOREIRA <bv3024679@AD.SBV.IFSP.EDU.BR>
2022-04-18 15:24:21 -0300

    Aula 18/04 - For

---
+++ CALC1/ex3.12.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n1;
+    int n2;
+    int n1True;
+    int par;
+
+    printf("N1: ");
+    scanf("%d", &n1);
+
+    printf("N2: ");
+    scanf("%d", &n2);
+
+    n1True = (n1 <= n2) ? n1 : n2;
+    n2 = (n1 <= n2) ? n2 : n1;
+
+    for(int i = n1True; i <= n2; i++){
+        if(i%2 == 0){
+            par++;
+        }
+    }
+
+    printf("Numeros pares entre %d e %d: %d", n1True, n2, par);
+
+    return 0;
+}
