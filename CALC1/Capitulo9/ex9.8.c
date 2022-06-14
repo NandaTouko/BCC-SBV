1c8c90e4caf438f9610034ce1582b09df933ec2f
AMANDA MOREIRA <bv3024679@AD.SBV.IFSP.EDU.BR>
2022-06-14 16:28:42 -0300

    Foi o exercíciooooo

--- CALC1/Capitulo9/ex9.8.c
+++ CALC1/Capitulo9/ex9.8.c
@@ -5,6 +5,8 @@
 int main() {
     char string[3][41]; // 3 strings de 40 caracteres + um para \0
     int comp1;
+    int comp2;
+    int comp3;
 
     for(int i = 0; i < 3; i++){
         printf("String %d: ", i+1);
@@ -14,9 +16,21 @@ int main() {
     }
 
     comp1 = strcmp(string[0], string[1]);
+    comp2 = strcmp(string[1], string[2]);
+    comp3 = strcmp(string[2], string[0]);
 
-    if(comp1 < 0){
-        printf("%s, %s", string[0], string[1])
+    if(comp1 <= 0 && comp2 <= 0){
+        printf("%s, %s e %s", string[0], string[1], string[2]);
+    }else if(comp3 >= 0 && comp2 >= 0){
+        printf("%s, %s e %s", string[0], string[2], string[1]);
+    }else if(comp2 <= 0 && comp3 >= 0){
+        printf("%s, %s e %s", string[1], string[2], string[0]);
+    }else if(comp1 >= 0 && comp3 >= 0){
+        printf("%s, %s e %s", string[1], string[0], string[2]);
+    }else if(comp2 >= 0 && comp1 >= 0){
+        printf("%s, %s e %s", string[2], string[1], string[0]);
+    }else if(comp3 <= 0 && comp1 <= 0){
+        printf("%s, %s e %s", string[2], string[0], string[1]);
     }
 
     return 0;
