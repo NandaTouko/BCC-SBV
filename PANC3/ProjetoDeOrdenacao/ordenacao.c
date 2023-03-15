a6bd7d47b29b84fa8019df1c99795156f70063aa
AMANDA MOREIRA <bv3024679@AD.SBV.IFSP.EDU.BR>
2023-03-15 18:29:26 -0300

    Aula 15/03/2023

--- PANC3/ProjetoDeOrdenacao/ordenacao.c
+++ PANC3/ProjetoDeOrdenacao/ordenacao.c
@@ -35,3 +35,43 @@ void insertionSort(int arrayOriginal[], int n) {
         arrayOriginal[i+1] = chave;
     }
 }
+
+/*
+    Função insertionSortAlternativo(): ordena o array especificando
+    o metodo de ordenação por inserção com dois for
+*/
+void insertionSortAlternativo(int arrayOriginal[], int n){
+    for(int i = 1; i < n; i++){
+        for(int j = i; j > 0; j--){
+            if(arrayOriginal[j] < arrayOriginal[j-1]){
+                int chave = arrayOriginal[j-1];
+                arrayOriginal[j-1] = arrayOriginal[j];
+                arrayOriginal[j] = chave;
+            }
+        }
+    }
+}
+
+/*
+    Funcao copiaValoresEntreArrays(): copia valores inteiros entre arrays
+*/
+void copiaValoresEntreArrays(int arrayOriginal[], int arrayA[], int n){
+    for(int i = 0; i < n; i++){
+        arrayA[i] = arrayOriginal[i];
+    }
+}
+
+/*
+    Funcao mergeSortRecursivo(): Ordena array especifico utilizando o
+    merge sort recursivo com paradigma da divisao e conquista
+*/
+void mergeSortRecursivo(int arrayA[], int inicio, int fim){
+}
+
+/*
+    Funcao intercalaSemSentinela(): Metodo auxiliar do merge sort para realizar
+    a intercalacao de valores
+*/
+void intercalaSemSentinela(int arrayA[], int inicio, int meio, int fim){
+}
+
--- PANC3/ProjetoDeOrdenacao/ordenacao.h
+++ PANC3/ProjetoDeOrdenacao/ordenacao.h
@@ -4,5 +4,9 @@
 void gerandoArrayAleatorio(int arrayOriginal[], int n);
 void imprimeArray(int arrayOriginal[], int n);
 void insertionSort(int arrayOriginal[], int n);
+void insertionSortAlternativo(int arrayOriginal[], int n);
+void copiaValoresEntreArrays(int arrayOriginal[], int arrayA[], int n);
+void mergeSortRecursivo(int arrayA[], int inicio, int fim);
+void intercalaSemSentinela(int arrayA[], int inicio, int meio, int fim);
 
 #endif // ORDENACAO_H
