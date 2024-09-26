bd2221fd7e69643cf4491cc3e3d33de4346320cd
AMANDA MOREIRA <bv3024679@ad.sbv.ifsp.edu.br>
2024-09-26 16:15:56 -0300

    Explicação do capítulo 3

---
+++ IDSW6_Introducao_Desenvolvimento_Web/Capitulo3/Aula3/src/java/entidades/Produto.java
@@ -0,0 +1,17 @@
+package entidades;
+
+/**
+ *
+ * @author bv3024679
+ */
+public class Produto {
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
