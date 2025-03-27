ca73fc7468a6c1147beaf2929c11172a77a205d7
Amanda Moreira <nandamoreira945@gmail.com>
2025-03-27 14:19:28 -0300

    Exercício de Quebrar Template

---
+++ DSOC7_Desenvolvimento_Web_II/ExQuebrarTemplate/App/Routes/Routes_Alunos.php
@@ -0,0 +1,29 @@
+<?php
+
+$routes['LandingPage'] = array(
+    'route' => '/',
+    'controller' => 'AlunosController',
+    'action' => 'index'
+);
+
+//Carrega a Dashboard de Alunos
+$routes['Dashboard_alunos'] = array(
+    'route' => '/dashboard/alunos',
+    'controller' => 'AlunosController',
+    'action' => 'dashboard'
+);
+
+//Carrega o Formulário de Cadastro de Alunos
+$routes['cadastrar_alunos'] = array(
+    'route' => '/dashboard/alunos/cadastrar',
+    'controller' => 'AlunosController',
+    'action' => 'cadastrar'
+);
+
+
+//Salvar Cadastro de Alunos
+$routes['Salvar_Cadastro_alunos'] = array(
+    'route' => '/dashboard/alunos/salvar',
+    'controller' => 'AlunosController',
+    'action' => 'salvar'
+);
---
+++ DSOC7_Desenvolvimento_Web_II/ExQuebrarTemplate/App/View/landingpage.php
@@ -0,0 +1,8 @@
+<?php
+
+include 'includes/landingpage/header.php';
+include 'includes/landingpage/menu.php';
+$this->content(); //aqui será carregado o conteúdo da página
+include 'includes/landingpage/footer.php';
+
+?>
